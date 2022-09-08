50c982755534009c9e5b80e2eb81c0fd1d46a59f
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-08 15:22:08 +0530

    created abstract method draw

---
+++ AbstarctUsage.cs
@@ -0,0 +1,79 @@
+namespace ConsoleApp16.WhyAbstract
+{
+    //Common Code
+    //Variation Code
+
+    
+    public class Program
+    {
+        static void Main()
+        {
+    
+            SearchWindow _searchWindow = new SearchWindow();
+            _searchWindow.Draw();
+        }
+    }
+
+
+    public abstract class Control
+    {
+        public string ControlId, ToolTip;
+        public double Height, Width;
+
+        abstract void Draw()
+        {
+
+        } 
+    }
+
+    public class TextBox:Control
+    {
+        
+        public string Text;
+        public void Draw()
+        {
+
+            Console.WriteLine("TextBox Draw");
+        }
+        
+    }
+    public class Button:Control
+    {
+        
+        public string Caption;
+        public void Draw()
+        {
+            Console.WriteLine("Button Draw");
+        }
+    }
+    public class Window:Control
+    {
+        
+        public string Title;
+        public void Draw()
+        {
+            // Window Draw Code
+            Console.WriteLine("Window Draw");
+        }
+    }
+
+    public class SearchWindow : Window
+    {
+        TextBox _textBox = new TextBox();
+        Button _button = new Button();
+
+        public override void Draw()
+        {
+            _textBox.Draw();
+            _button.Draw();
+        }
+
+    }
+
+
+
+
+
+
+
+}
